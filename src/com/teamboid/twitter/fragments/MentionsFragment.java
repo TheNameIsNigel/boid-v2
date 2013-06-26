649bd98fb5ce4892f9a7244ff9e4e9e63e78fbc3
Aidan Follestad <drummer.aidan@gmail.com>
2013-06-26 17:15:17 -0500

    Pagination fix.

--- src/com/teamboid/twitter/fragments/MentionsFragment.java
+++ src/com/teamboid/twitter/fragments/MentionsFragment.java
@@ -60,7 +60,7 @@ public class MentionsFragment extends FeedFragment<Status> {
         BoidAdapter adapt = getAdapter();
         if (adapt.getCount() > 0) {
             // Get tweets older than the oldest tweet in the adapter
-            paging.setSinceId(adapt.getItemId(adapt.getCount() - 1));
+            paging.setMaxId(adapt.getItemId(adapt.getCount() - 1));
         }
         return BoidApp.get(getActivity()).getClient().getMentionsTimeline(paging).toArray(new Status[0]);
     }
--- src/com/teamboid/twitter/fragments/TimelineFragment.java
+++ src/com/teamboid/twitter/fragments/TimelineFragment.java
@@ -60,7 +60,7 @@ public class TimelineFragment extends FeedFragment<Status> {
         BoidAdapter adapt = getAdapter();
         if (adapt.getCount() > 0) {
             // Get tweets older than the oldest tweet in the adapter
-            paging.setSinceId(adapt.getItemId(adapt.getCount() - 1));
+            paging.setMaxId(adapt.getItemId(adapt.getCount() - 1));
         }
         return BoidApp.get(getActivity()).getClient().getHomeTimeline(paging).toArray(new Status[0]);
     }
