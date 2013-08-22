a9ebddadc5f21a96831d6e76884021003ff6d4cc
Aidan Follestad <drummer.aidan@gmail.com>
2013-08-22 17:33:49 -0500

    Cache size is limited.

--- src/com/afollestad/twitter/fragments/base/BoidListFragment.java
+++ src/com/afollestad/twitter/fragments/base/BoidListFragment.java
@@ -5,9 +5,9 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.ListView;
+import com.afollestad.silk.cache.CacheLimiter;
 import com.afollestad.silk.cache.SilkCacheManager;
 import com.afollestad.silk.cache.SilkComparable;
-import com.afollestad.silk.cache.TrimMode;
 import com.afollestad.silk.fragments.SilkLastUpdatedFragment;
 import com.afollestad.twitter.BoidApp;
 import com.afollestad.twitter.R;
@@ -39,7 +39,7 @@ public abstract class BoidListFragment<T extends SilkComparable> extends SilkLas
 
     @Override
     protected SilkCacheManager<T> onCacheInitialized(SilkCacheManager<T> cache) {
-        cache.setSizeLimit(750, TrimMode.BOTTOM);
+        cache.setLimiter(new CacheLimiter(750, CacheLimiter.TrimMode.BOTTOM));
         return cache;
     }
 
