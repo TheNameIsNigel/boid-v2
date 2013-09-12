d4f08a86a1e59e1b7f41fa97e2333ef2ff6c6d52
Aidan Follestad <drummer.aidan@gmail.com>
2013-09-11 20:42:24 -0500

    Theming updates

--- src/com/afollestad/twitter/views/BoidButton.java
+++ src/com/afollestad/twitter/views/BoidButton.java
@@ -1,6 +1,7 @@
 package com.afollestad.twitter.views;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.widget.Button;
 import com.afollestad.twitter.R;
@@ -29,9 +30,11 @@ public class BoidButton extends Button {
 
     private void init() {
         setTextColor(getResources().getColor(android.R.color.white));
-        setBackgroundResource(R.drawable.boid_button);
         int padding = getResources().getDimensionPixelSize(R.dimen.borderless_padding);
         setPadding(padding, padding, padding, padding);
+        TypedArray ta = getContext().obtainStyledAttributes(new int[]{R.attr.boid_button});
+        setBackground(ta.getDrawable(0));
+        ta.recycle();
     }
 
     @Override
